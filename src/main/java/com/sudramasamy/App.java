96ccd6d7031379a5c880efdb29e773bf87649a5f
sudr <to_sud@yahoo.com>
2012-05-21 21:41:32 -0500

    initial commit with testng, gauva and hamcrest

---
+++ src/main/java/com/sudramasamy/App.java
@@ -0,0 +1,9 @@
+package com.sudramasamy;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/sudramasamy/AppTest.java
@@ -0,0 +1,15 @@
+package com.sudramasamy;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+import org.testng.annotations.Test;
+
+@Test
+public class AppTest 
+{
+
+    public void canary()
+    {
+        assertTrue( true );
+    }
+}
